2e7c0cc62732622864cdf6f1eec22d9a3ec98238
Pedro Carvalho <pedrosophbc@gmail.com>
2020-10-02 00:56:50 -0300

    Vinculando atributos do form

--- src/app/template-driven-form/template-driven-form.component.ts
+++ src/app/template-driven-form/template-driven-form.component.ts
@@ -1,5 +1,17 @@
 import { Component, OnInit } from '@angular/core';
 
+export interface Cliente{
+  nome: string;
+  sobrenome: string;
+  nascimento: Date;
+  estado:string;
+  cidade: string;
+  rua: string;
+  telefone:string;
+  telefone2:string;
+}
+
+
 @Component({
   selector: 'app-template-driven-form',
   templateUrl: './template-driven-form.component.html',
@@ -7,9 +19,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TemplateDrivenFormComponent implements OnInit {
 
+  cliente:Cliente = {
+      nome: "",
+      sobrenome: "",
+      nascimento: new Date, 
+      estado:"AM",  
+      cidade: "", 
+      rua:"",
+      telefone: "",
+      telefone2: ""
+    }
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  onSubmit(){
+    console.log(this.cliente)
   }
 
 }
